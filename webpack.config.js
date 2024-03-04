b74496bbfa9f2dd07d99ff3f0f824003aeff36e5
m4zin <mazin.sherif@gmail.com>
2024-03-04 15:43:56 +0400

    task: set up webpack & jest

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/scripts/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'head',
+            scriptLoading: 'defer'
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    devServer: {
+        static: './dist/index.html',
+        port: 8080,
+        hot: true,
+    }
+};
